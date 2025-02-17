c7ee8d5d94c5b8c7c5fa6e46bcb4e819646c3b2e
ReddyHoma <22btrco059@jainuniversity.ac.in>
2025-02-17 00:01:10 -0500

    Modified reducers to toggle recipes

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -1,15 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  favoriterecipes: [], // Updated to handle favorite articles
+    favoriterecipes: [], // Updated to handle favorite articles
 };
 
 const favoritesSlice = createSlice({
-  name: "favorites",
-  initialState,
-  reducers: {
-   
-  },
+    name: "favorites",
+    initialState,
+    reducers: {
+        toggleFavorite: (state, action) => {
+            const article = action.payload; // Expecting the entire article object
+            const existingIndex = state.favoriterecipes.findIndex(
+                (item) => item.id === recipe.idFood // Assuming articles have an 'id' property
+            );
+            if (existingIndex >= 0) {
+                state.favoriterecipes.splice(existingIndex, 1); // Remove from favorites
+            } else {
+                state.favoriterecipes.push(recipe); // Add to favorites
+            }
+        },
+
+
+    },
 });
 
 export const { toggleFavorite } = favoritesSlice.actions;
